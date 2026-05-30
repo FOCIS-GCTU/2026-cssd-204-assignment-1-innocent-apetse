05f4b0ce549238164f74b3e84be26da3d5ab7e9e
innocent-apetse <2425403484@live.gctu.edu.gh>
2026-05-30 15:57:46 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,31 @@
+# File: initials.py
+# Description: Print out my initials.
+# Assignment Number: 2
+#
+# Name: INNOCENT APETSE
+# STUDENT ID: 2425403484
+# Email: 2425403484@live.gctu.edu.gh
+# Grader: Augustus Buckman
+#
+# On my honor, Innocent Apetse , this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
-
-
-
+    # Print initials in stylized large letters
+    print()
+    print("...IAK")
+    print()
+    print("...IIIIIIIIIIII...**........AA........**...KK........KK...")
+    print("........II........**......AA..AA......**...KK......KK.....")
+    print("........II...............AA....AA..........KK....KK.......")
+    print("........II..............AA......AA.........KK..KK.........")
+    print("........II.............AAAAAAAAAAAA........KKKK...........")
+    print("........II.............AA........AA........KKKK...........")
+    print("........II.............AA........AA........KK..KK.........")
+    print("........II.............AA........AA........KK....KK.......")
+    print("........II........**...AA........AA...**...KK......KK.....")
+    print("...IIIIIIIIIIII...**...AA........AA...**...KK........KK...")
+    print()
 main()
+
